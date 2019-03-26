c6b75ac4bae352810f8c92dc0801a76ada0a14f1
Humphrey Hu <1443539380@qq.com>
2019-03-26 19:43:31 +0800

    Initial commit 《代码重构之道》第一版：影片租售程序

---
+++ src/main/java/com/hhp/rental/Movie.java
@@ -0,0 +1,36 @@
+package com.hhp.rental;
+
+/**
+ * @author: Humphrey Hu
+ * @date: 19/03/26
+ * @description:
+ */
+public class Movie {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String title;
+    private int priceCode;
+
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.priceCode = priceCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPriceCode() {
+        return priceCode;
+    }
+
+    public void setPriceCode(int priceCode) {
+        this.priceCode = priceCode;
+    }
+}
---
+++ src/main/java/com/hhp/rental/Rental.java
@@ -0,0 +1,24 @@
+package com.hhp.rental;
+
+/**
+ * @author: Humphrey Hu
+ * @date: 19/03/26
+ * @description:
+ */
+public class Rental {
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+}
